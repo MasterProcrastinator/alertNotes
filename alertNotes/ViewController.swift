ea97daa055f1a816f181bf3511e71013b8e567f2
Alvin <thisiseasytoremember1224@gmail.com>
2023-10-25 09:07:35 -0500

    finish

--- alertNotes/ViewController.swift
+++ alertNotes/ViewController.swift
@@ -9,11 +9,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nameTextfieldOutlet: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    
+    @IBAction func submitAction(_ sender: UIButton) {
+        //Create Alert Controller
+        let alert = UIAlertController(title: "Error", message: "Not allowed", preferredStyle: .alert)
+        
+        //Create Alert Actions
+        let okAction = UIAlertAction(title: "Ok", style: .default, handler: nil)
+        
+        //Add Actions
+        alert.addAction(okAction)
+        
+        
+        //present alert controllers
+        present(alert, animated: true, completion: nil)
+        
+        
+    }
 
+    
 
 }
 
